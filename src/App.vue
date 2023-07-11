16b4b915020ce995ca50fba4cd3b48047ff7d02d
CR2-01\iSpan <chuanhui603@gmail.com>
2023-07-11 16:59:45 +0800

    模板更新  刪除一些不必要檔案

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import NavTop from '@/components/NavTop.vue'
-import HeroSection from '@/components/HeroSection.vue'
+
 </script>
-
 <template> 
- <!-- <div id="preloder">
-      <div class="loader"></div>
-  </div> -->
-    <NavTop />
-    <HeroSection/>
-  
-      <!-- <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav> -->
-    
-  
-
-  <!-- <RouterView /> -->
+ 
 </template>
