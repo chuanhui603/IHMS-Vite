0d225ee45a927149dc05fb399913c3b0cb708f85
Hui <chuanhui603@gmail.com>
2023-07-13 02:16:59 +0800

    重建模板 測試匯入App.vue

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+(function () {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            var spinnerElement = document.getElementById('spinner');
+            if (spinnerElement) {
+                spinnerElement.classList.remove('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    var script = document.createElement('script');
+    script.src = '../lib/wow/wow.min.js';
+    script.onload = function () {
+        // Initiate the wowjs
+        new WOW().init();
+    };
+    document.body.appendChild(script);
+
+    // Fixed Navbar
+    window.addEventListener('scroll', function () {
+        if (window.innerWidth < 992) {
+            if (window.scrollY > 45) {
+                document.querySelector('.fixed-top').classList.add('bg-white', 'shadow');
+            } else {
+                document.querySelector('.fixed-top').classList.remove('bg-white', 'shadow');
+            }
+        } else {
+            if (window.scrollY > 45) {
+                document.querySelector('.fixed-top').classList.add('bg-white', 'shadow');
+                document.querySelector('.fixed-top').style.top = '0';
+            } else {
+                document.querySelector('.fixed-top').classList.remove('bg-white', 'shadow');
+                document.querySelector('.fixed-top').style.top = '0';
+            }
+        }
+    });
+
+
+    // Back to top button
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 300) {
+            document.querySelector('.back-to-top').style.display = 'block';
+        } else {
+            document.querySelector('.back-to-top').style.display = 'none';
+        }
+    });
+    // document.querySelector('.back-to-top').addEventListener('click', function () {
+    //     window.scrollTo({ top: 0, behavior: 'smooth' });
+    // });
+})();
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-import inject from '@rollup/plugin-inject';
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    inject({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'windows.jQuery': 'jquery',
-    }),
-      
   ],
   resolve: {
     alias: {
