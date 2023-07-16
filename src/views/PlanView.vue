dc83b780538eaa40ff60e7d58ba0b32b0dd391f8
Hui <chuanhui603@gmail.com>
2023-07-16 23:06:19 +0800

    添加chart.js圖表測試

--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,26 +1,25 @@
 <script setup>
 import plansidebar from '../components/PlansideBar.vue'
-
-const loadProducts = async (e)=>{
+import BarChart from '../components/BarChart.vue'
+const loadProducts = async (e) => {
     console.log(e)
-     const res = await fetch(`https://localhost:7127/api/Plans/${e}`)
-     const datas = await res.json()    
-      console.log(datas)
+    const res = await fetch(`https://localhost:7127/api/Plans/${e}`)
+    const datas = await res.json()
+    console.log(datas)
 
-   }
+}
 
-
-    
 
 </script>
     
 <template>
-    <div class="container-xxl" style="height: 100vh;background-color: var(--secondary);margin-top: 79px;">
+    <div class="container-xxl" style="height: 100vh;margin-top: 79px;">
         <!-- 內容  -->
-        <div class="row" >
+        <div class="row">
             <!-- sidebar -->
-           <plansidebar></plansidebar>
-            <div class="col-lg-9" style="background-color: var(--primary)">
+            <plansidebar></plansidebar>
+            <div class="col-lg-9" >
+              <BarChart></BarChart>
                 <button type="button" @click="loadProducts(1)">AJAX測試</button>
             </div>
         </div>
@@ -28,10 +27,11 @@ const loadProducts = async (e)=>{
 </template>
 
 <style scoped>
-.col-lg-3{
-    width:20%;
+.col-lg-3 {
+    width: 20%;
 }
-.col-lg-9{
-    width:80%;
+
+.col-lg-9 {
+    width: 80%;
 }
 </style>
