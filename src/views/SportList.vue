2367970d9bb5bc922ea387aea752d4c218d1f354
Hui <chuanhui603@gmail.com>
2023-07-25 01:21:31 +0800

    修正sportDetail AJAX

--- src/views/SportList.vue
+++ src/views/SportList.vue
@@ -1,26 +1,31 @@
 <template>
-        <form method="post" @submit.prevent="detailEdit">
+        <form method="post" @submit.prevent="detailEdit(1)">
                 <div>
-                        <input type="text" placeholder="運動名稱" v-model="sportdetail.Dname" required>
-                        <section type="text" placeholder="運動分類" required></section>
-                        <input type="text" placeholder="組數" v-model="sportdetail.Calories" required>
-                        <input type="text" placeholder="運動時間" v-model="sportdetail.Calories" required>
-                        <input type="file" @change="ImgAdd" multiple>                     
-                        <textarea v-model="sportdetail.Decription"></textarea>
-                        <button type="submit">修改</button>
-                        <button @click="cancelReturn">修改</button>
+                        <input type="text" placeholder="運動名稱" v-model="sportdetail.Sname" required>
+                        <select type="text" v-model="sportdetail.Type" required>
+                                <option value="健身">健身</option>
+                                <option value="有氧">有氧</option>
+                        </select>
+
+                        <input type="text" placeholder="組數" v-model="sportdetail.Frequency" required>
+                        <input type="text" placeholder="運動時間" v-model="sportdetail.Sporttime" required>
+                        <input type="file" @change="ImgAdd" multiple>
+                        <textarea v-model="sportdetail.Description"></textarea>
+                        <button type="submit" >修改</button>
+                        <button @click="cancelReturn">返回</button>
                 </div>
         </form>
+
+
         <div v-if="sportdetail.Img.length > 0">
-                                <h5>已選擇的圖片檔案：</h5>
-                                <div v-for="(image, index) in imgList" :key="index">
-                                        <p>{{ sportdetail.Img[index].name }}</p>
-                                        <img :src="image" width="100" height="100">
-                                        <button @click="imgDelete(index)">刪除相片</button>
-                                </div>
+                <h5>已選擇的圖片檔案：</h5>
+                <div v-for="(image, index) in imgList" :key="index">
+                        <p>{{ sportdetail.Img[index].name }}</p>
+                        <img :src="image" width="100" height="100">
+                        <button @click="imgDelete(index)">刪除相片</button>
+                </div>
 
-                        </div>
-
+        </div>
 </template>
         
 <script setup>
@@ -35,7 +40,7 @@ const sportdetail = ref({
         Type: '',
         Sporttime: 0,
         Frequency: 0,
-        Decription: '',
+        Description: '',
         Img: [],
         Registerdate: date.toLocaleDateString(),
 })
@@ -46,6 +51,11 @@ const loadMethod = async (sportId) => {
         data.value = await res.json()
 }
 loadMethod(1)
+
+const typeChange = (e) => {
+        console.log(e.target.value)
+        sportdetail.value.type = e.target.value
+}
 
 const ImgAdd = (e) => {
         sportdetail.value.Img.push(...e.target.files)
@@ -58,31 +68,42 @@ const ImgAdd = (e) => {
                                 imgUrl.value.push(event.target.result);
                         };
                         reader.readAsDataURL(file);
-                } 
-                
+                }
+
         }
         console.log(imgUrl)
 }
 
 const imgDelete = (index) => {
-        sportdetail.value.Img.splice(index,1)
-        imgUrl.value.splice(index,1)
+        sportdetail.value.Img.splice(index, 1)
+        imgUrl.value.splice(index, 1)
 }
 
-const imgList = computed(() => 
+const imgList = computed(() =>
         sportdetail.value.Img.map((_, index) => imgUrl.value[index]
-));
+        ));
 
-const detailEdit = async (DetailID) => {
+const detailEdit = async () => {
 
+        const formData = new FormData()
+        formData.append('SportDetailId', sportdetail.value.SportDetailId)
+      
+        formData.append('SportId', sportdetail.value.SportId)
+        formData.append('Sname', sportdetail.value.Sname)
+        formData.append('Type', sportdetail.value.Type)
+        formData.append('Sporttime', sportdetail.value.Sporttime)
+        formData.append('Frequency', sportdetail.value.Frequency)
+        formData.append('Description', sportdetail.value.Description)
+        sportdetail.value.Img.forEach((img) => {
+                formData.append('Img', img)
+        })
+        formData.append('Registerdate', sportdetail.value.Registerdate)
 
-        const API_URL = `https://localhost:7127/api/plans/dietdetail/${DetailID}/edit`
+        
+        const API_URL = `https://localhost:7127/api/plans/sportdetail/edit`
         const res = await fetch(API_URL, {
-                method: 'PUT',
-                headers: {
-                        'content-type': 'application/json'
-                },
-                body: JSON.stringify(sportdetail.value),
+                method: 'PUT',             
+                body: formData,
         }).catch(err => {
                 console.log(err);
         }
